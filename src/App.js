4cfd999167b823ceba72a52511f1d4dcf7260304
zhaoyang lin <44690950+zhaoyanglin@users.noreply.github.com>
2019-06-05 21:39:51 -0500

    Fix host issue

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css'
 class App extends Component {
 
   state = {
-    endpoint: "ws://real-time-calculator.herokuapp.com:4001",
+    endpoint: "https://real-time-calculator.herokuapp.com:4001",
     firstValue: '',
     secondValue: '',
     result: '',
