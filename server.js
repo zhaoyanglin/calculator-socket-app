1098d013e041303eceeb8220eb8805d55eade3ff
zhaoyanglin <zhaoyanglin@msn.com>
2019-06-05 21:08:10 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express')
+const http = require('http')
+const socketIO = require('socket.io')
+
+
+const port = 4001
+
+const app = express()
+
+app.use(express.static('build'));
+
+
+const server = http.createServer(app)
+
+
+const io = socketIO(server)
+
+class CalculationModel {
+    constructor(firstValue, secondValue, result, operation) {
+        this.firstValue = firstValue;
+        this.secondValue = secondValue;
+        this.result = result;
+        this.operation = operation;
+    }
+
+    toString() {
+        return `${this.firstValue} ${this.operation} ${this.secondValue} = ${this.result}`
+    }
+}
+
+let calculations = [];
+
+
+io.on('connection', socket => {
+
+    socket.on('add calculation', (event) => {
+console.log('this is the event=============', event);
+
+        const newCalc = new CalculationModel(event.firstValue, event.secondValue, event.result, event.operation).toString()
+
+        if(calculations.length < 10) {
+            calculations.push(newCalc)
+        } else {
+            calculations.shift()
+            calculations.push(newCalc)
+        }
+
+        io.sockets.emit('calc', calculations.reverse())
+    })
+
+    socket.on('new user', () => {
+        io.sockets.emit('calc', calculations.reverse())
+    })
+
+    // disconnect is fired when a client leaves the server
+    socket.on('disconnect', () => {
+        console.log('user disconnected')
+    })
+})
+
+server.listen(port, () => console.log(`Listening on port ${port}`))
--- src/App.js
+++ src/App.js
@@ -1,26 +1,146 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import socketIOClient from "socket.io-client";
+import './App.css'
+class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  state = {
+    endpoint: "localhost:4001",
+    firstValue: '',
+    secondValue: '',
+    result: '',
+    operation: '',
+    calculations: [],
+  };
+
+  componentDidMount = () => {
+    const socket = socketIOClient(this.state.endpoint);
+    socket.emit('new user')
+    socket.on('calc', (array) => {
+      this.setState({
+        calculations: array,
+      })
+    })
+  }
+
+  // sending sockets
+  send = () => {
+    const socket = socketIOClient(this.state.endpoint);
+    socket.emit('add calculation', this.state)
+  }
+  ///
+
+  //input 
+  handleChangFor = (key) => (event) => {
+    this.setState({
+      [key]: event.target.value
+    })
+  }
+  //
+  //operation 
+  onChangeOperation = (key) => {
+    this.setState({
+      operation: key,
+    })
+  }
+  //
+  //clear all 
+  onclickClear = () => {
+    this.setState({
+      firstValue: '',
+      secondValue: '',
+      result: '',
+      operation: '',
+    })
+  }
+  //
+// calculation
+  calculate = () => {
+    if (this.state.firstValue === '' || this.state.secondValue === '') {
+      alert("PLEASE FILL IN BOTH INPUTS BEFORE CONTINUING!!!!!!!!")
+    }
+    else if (this.state.operation === '+') {
+      this.setState({
+        result: Number(this.state.firstValue) + Number(this.state.secondValue)
+      }, () => {
+        this.send()
+      })
+
+    } else if (this.state.operation === '-') {
+      this.setState({
+        result: this.state.firstValue - this.state.secondValue
+      }, () => {
+        this.send()
+      })
+
+    } else if (this.state.operation === '*') {
+      this.setState({
+        result: this.state.firstValue * this.state.secondValue
+
+      }, () => {
+        this.send()
+      })
+
+    } else if (this.state.operation === '/') {
+      this.setState({
+        result: this.state.firstValue / this.state.secondValue
+
+      }, () => {
+          this.send()
+      })
+    }
+  }
+  //
+
+  render() {
+    // get updated list
+    let TopTenList = null;
+    TopTenList = this.state.calculations.map((data, i) => {
+      return (
+        <div>
+          <li key={i} >{data}</li>
+          <br />
+        </div>
+      )
+    })
+
+    return (
+      <div className="App" >
+
+        <div id='calDiv'>
+
+          <div id='firstInputDiv'>
+            <input type='number'id='fisrtVal' placeholder='first value' onChange={this.handleChangFor('firstValue')} value={this.state.firstValue} />
+          </div>
+
+          <div id='operationButtonDiv'>
+            <button onClick={() => this.onChangeOperation('+')}>+</button>
+            <br />
+            <button onClick={() => this.onChangeOperation('-')}>-</button>
+            <br />
+            <button onClick={() => this.onChangeOperation('*')}>*</button>
+            <br />
+            <button onClick={() => this.onChangeOperation('/')}>/</button>
+          </div>
+
+          <div id='secondInputDiv'>
+            <input type='number' id='secondVal' placeholder='second value' onChange={this.handleChangFor('secondValue')} value={this.state.secondValue} />
+          </div>
+
+          <div id='lastDiv'>
+            <button onClick={() => this.calculate()} > = </button>
+            <button onClick={() => this.onclickClear()}>C</button>
+          </div>
+
+        </div>
+
+        <div id='listDiv'>
+          <ul>
+            {TopTenList}
+          </ul>
+        </div>
+
+      </div>
+    )
+  }
 }
-
 export default App;
